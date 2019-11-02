81e1c640f0fec2727e59f86e84e68c5416053dd9
Kumar Gaurav <kmrgaurav33@gmail.com>
2019-11-03 02:12:25 +0530

    intorduced cookie parser

--- src/app.js
+++ src/app.js
@@ -1,20 +1,24 @@
+// importing core dependencies
 const express = require('express');
 const app = express();
-const config = require('./config');
 
+// middlewares
+app.use(require('cookie-parser')('KUMAR'));
+
+// configs for application
+const config = require('./config');
 const port = process.env.PORT || config.PORT 
 
-const userRouter = require('./routers/user')
-const eventRouter = require('./routers/event')
 
 app.get('/', (req, res) => {
   res.send("Welcome to QZ-RED !!!")
 })
 
-app.use('/user', userRouter);
-app.use('/event', eventRouter);
+// routers
+app.use('/user',  require('./routers/user'));
+app.use('/event', require('./routers/event'));
 
-
+// application initialization
 app.listen(port, () => {
   console.log(`QZ-Red app started listening at port ${port}`)
 })
--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,47 @@ const userRouter = require('express').Router();
 
 userRouter.get('/', (req, res) => {
   res.send('Users route is live...');
+
+  // TODO
+  // if signed in -> send user details
+  // else -> send unauthorized status
+})
+
+userRouter.post('/signin', (req, res) => {
+  // TODO
+  // signin user
+})
+
+userRouter.get('/signout', (req, res) => {
+  // TODO
+  // signout user
+
+  // learning
+  console.log(['normal cookies', req.cookies])
+  console.log(['signed cookies', req.signedCookies])
+  res
+    .cookie('uuid', '1234', {
+      signed: true
+    })
+    .clearCookie('cookie-name-to-be-cleared')
+    .status(418)
+    .json({
+      status: 400,
+      body: {
+        message: 'Hi Batman'
+      }
+    })
+})
+
+userRouter.post('/singup', (req, res) => {
+  // TODO
+  // singup user
+})
+
+userRouter.post('/updateSettings', (req, res) => {
+  // TODO
+  // if signed in -> update user settings
+  // else -> send unauthorised status
 })
 
 module.exports = userRouter
